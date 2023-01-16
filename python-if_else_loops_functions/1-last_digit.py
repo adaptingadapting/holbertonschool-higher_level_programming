ebea43d9475ea4a9124e6e86704b1f8371b16af4
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-16 14:16:58 -0300

    1 hotfix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"The last digit of {number} is", end=" ")
+print(f"Last digit of {number} is", end=" ")
 
 if (number < 0):
         number = -number
