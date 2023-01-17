06e3705df7777962cdb055269836925fba55c3a0
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-17 12:29:04 -0300

    task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 
-if (__name__ == "__main__")
-        import calculator_1
-        a = 10
-        b = 5
-        print("{} + {} = {}", a, b, add(a, b))
-        print("{} - {} = {}", a, b, sub(a, b))
-        print("{} * {} = {}", a, b, mul(a, b))
-        print("{} / {} = {}", a, b, div(a, b))
+import calculator_1
+if (__name__ == "__main__"):
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
