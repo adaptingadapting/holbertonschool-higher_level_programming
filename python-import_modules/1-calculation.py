c6d3ed8a8c21f15d16a2c767d8dfd289844e4949
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-17 11:33:09 -0300

    task 1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+if (__name__ == "__main__")
+        import calculator_1
+        a = 10
+        b = 5
+        print("{} + {} = {}", a, b, add(a, b))
+        print("{} - {} = {}", a, b, sub(a, b))
+        print("{} * {} = {}", a, b, mul(a, b))
+        print("{} / {} = {}", a, b, div(a, b))
