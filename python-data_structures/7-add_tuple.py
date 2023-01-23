6b6ff85e265fa751bc5c41e446355ba03eff95de
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-23 14:18:43 -0300

    task 7 name hange

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    len_t_1 = len(tuple_a)
+    len_t_2 = len(tuple_b)
+    if len_t_1 < 2:
+        if len_t_1 < 1:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
+    if len_t_2 < 2:
+        if len_t_2 < 1:
+            tuple_b = (0, 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
