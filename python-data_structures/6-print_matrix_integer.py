93f452979dad5cfab1a594e6da47b7a13dd91105
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-23 12:57:27 -0300

    task 6 hotfix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,8 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if not j:
-                print("{}".format(matrix[i][j]), end="")
+                print("{:d}".format(matrix[i][j]), end="")
             else:
                 print(" ", end="")
-                print("{}".format(matrix[i][j]), end="")
+                print("{:d}".format(matrix[i][j]), end="")
         print()
