38b83ead0db64bee4fd7e39105e019d744ed6307
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-25 13:50:54 -0300

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print()
+    except:
+        print()
+        return i
+    return x
