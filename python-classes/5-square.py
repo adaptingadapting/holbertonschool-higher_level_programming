7fde9481d46fc74946f37ee710b06f2f7770a817
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-30 10:08:13 -0300

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" Module defines a square class """
+
+
+class Square:
+    """ Square class with private instance attr size, area method """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if not value >= 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
