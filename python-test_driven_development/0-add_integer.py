8c2d93f5f864872ee0b79947965a1c75759eca7d
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-30 19:01:51 -0300

    changed task 1

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,13 +3,12 @@
 """ Takes two ints a and returns their sum """
 
 
-def add_integer(a, b=98):
+def add_integer(a, b):
 
     """ If a doesnt exist or a or b is not int or float\
     raise an exception """
-
     if not a or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return int(a + b)
