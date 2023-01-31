9783ee3d96e23af2118574bf5e57c383b366a3c7
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-31 10:51:24 -0300

    task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -12,20 +12,31 @@ class test_function_valid(unittest.TestCase):
 
     def test_empty_list(self):
         empty_list = []
-        self.assertEqual(max_integer(empty_list), None)
+        self.assertIsNone(max_integer(empty_list))
 
     def test_no_arg_passed(self):
-        self.assertEqual(max_integer(), None)
+        self.assertIsNone(max_integer())
 
     def test_one_arg(self):
         new_list = [1]
         self.assertEqual(max_integer(new_list), 1)
 
     def test_wrong_type(self):
-        new_list = "string"
-        self.assertRaises(TypeError, max_integer, new_list)
-        self.assertRaises(TypeError, max_integer, {"hola": 2})
-        self.assertRaises(TypeError, max_integer, (0, 0))
-        self.assertRaises(TypeError, max_integer, 2.4)
-        self.assertRaises(TypeError, max_integer, [2, 3])
         self.assertRaises(TypeError, max_integer, None)
+        max_integer("hola")
+        new_list = ["hola", 2]
+        self.assertRaises(TypeError, max_integer, new_list)
+
+    def test_values(self):
+        new_list = [-1, -2, -3]
+        self.assertEqual(max_integer(new_list), -1)
+        new_list = [1, -1]
+        self.assertEqual(max_integer(new_list), 1)
+
+    def test_posiiton(self):
+        new_list = [23, 45, 12]
+        self.assertEqual(max_integer(new_list), 45)
+        new_list = [45, 23, 12]
+        self.assertEqual(max_integer(new_list), 45)
+        new_list = [12, 23, 45]
+        self.assertEqual(max_integer(new_list), 45)
