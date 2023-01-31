e080de2ff3b4e3e9119e0914c7577600d907c3f4
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-31 18:13:15 -0300

    task 101

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -31,4 +31,4 @@ def lazy_matrix_mul(m_a, m_b):
         raise TypeError("each row of m_b must be equal in size")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b cant multiply")
-    return np.multiply(m_a, m_b)
+    return numpy.matmul(m_a, m_b)
