20fad2cc7c2b755855cbd71bcadcc518a100fbd2
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-30 21:58:46 -0300

    task 0

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Module that prints your name """
+
+def say_my_name(first_name, last_name=""):
+    if type(first_name) != str or first_name == None:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
