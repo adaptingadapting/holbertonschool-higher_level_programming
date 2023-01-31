0d59ea84022662c92703b11402afe5b413371038
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-31 19:30:32 -0300

    task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+""" module now also ass a few more features to the class """
+
+
+class Rectangle:
+    """ class now added area and perimeter methods """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if not value >= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if not value >= 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return self.__height * 2 + self.__width * 2
