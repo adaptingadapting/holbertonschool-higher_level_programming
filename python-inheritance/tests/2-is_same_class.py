a5721d460308c9e5b93a811484442a2d505055c8
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-02-06 14:26:58 -0300

    task

---
+++ python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module for creating a class """
+
+
+def is_same_class(obj, a_class):
+    """ returns the result of is operaiton on two things """
+    return type(obj) is a_class
