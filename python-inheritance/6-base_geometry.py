8850daa59c37ee77cb0594f15817c35c5e7ab16e
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-02-07 09:37:42 -0300

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module now has area method """
+
+
+class BaseGeometry:
+    """ area method added to class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
