48a54510888359ef9840462556d70587a60aad9b
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-02-07 15:30:10 -0300

    task 12 work in progress

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module for creating the pascals triangle """
+
+
+def pascal_triangle(n):
+    """ pascal triangle with recursion """
+
+    def recursion(n):
+        
+    if n <= 0:
+        return []
+    return recursion()
+
+        
