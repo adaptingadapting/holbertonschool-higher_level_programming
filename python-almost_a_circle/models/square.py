509aaf0cd8fb5ebd80f6ff147622068882c118b4
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-02-15 13:43:30 -0300

    all finieshed version square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,21 +8,31 @@ class Square(Rectangle):
     """ square class has methods and attributes :) """
 
     def __init__(self, size, x=0, y=0, id=None):
+        """ docu """
+
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """ docu """
+
         return self.width
 
     @size.setter
     def size(self, value):
+        """ docu """
+
         self.width = value
         self.height = value
 
     def __str__(self):
+        """ docu """
+
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
     def update(self, *args, **kwargs):
+        """ docu """
+
         if args and len(args) != 0:
             i = 0
             for arg in args:
@@ -47,4 +57,6 @@ class Square(Rectangle):
                     self.y = value
 
     def to_dictionary(self):
+        """ docu """
+
         return dict(id=self.id, x=self.x, size=self.size, y=self.y)
