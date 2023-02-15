ccc8aac7a8174ab4e5705b2ac339655ca048f561
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-02-15 10:56:49 -0300

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,13 +5,19 @@
 class Base:
     """ class is defined here """
 
-    
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        
+
+    def to_json_string(list_dictionaries):
+
+        import json
+
+        if type(list_dictionaries) is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
